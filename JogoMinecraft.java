6ef868651307cad8337f8795cf0f1ae269336659
thou <thou-vow>
2025-05-15 10:07:35 -0300

    feat(p1): adiciona jogador construtor

--- JogoMinecraft.java
+++ JogoMinecraft.java
@@ -5,29 +5,45 @@ public class JogoMinecraft {
     var gerador = new Random();
 
     var alex = new JogadorMinecraft("Alex");
+    JogadorMinecraft steve = new JogadorMinecraft("Steve Construtor");
 
-    while (alex.estaVivo()) {
-      switch (gerador.nextInt(3)) {
-          case 0:
-              alex.minerar();
-              break;
-          case 1:
-              alex.coletarMadeira();
-              break;
-          default:
-              alex.construir();
+    while (alex.estaVivo() || steve.estaVivo()) {
+      if (alex.estaVivo()) {
+        executarAcaoAleatoria(alex, gerador, new int[] {1,1,1});
+        if (gerador.nextInt(4) == 0) {
+          alex.levarDano();
+        }
+        System.out.println(alex);
       }
 
-      if (gerador.nextInt(4) == 0) {
-        alex.levarDano();
+      if (steve.estaVivo()) {
+        executarAcaoAleatoria(steve, gerador, new int[] {1,3,6});
+        if (gerador.nextInt(4) == 0) {
+          steve.levarDano();
+        }
+        System.out.println(steve);
       }
-
-      System.out.println(alex);
+      
       System.out.println("============");
-
       Thread.sleep(5000);
     }
 
     System.out.println("GAME OVER");
+  }
+
+  private static void executarAcaoAleatoria(JogadorMinecraft jogador, Random gerador, int[] pesos) {
+    int total = 0;
+    for (int i = 0; i < pesos.length; i++) {
+        total += pesos[i];
+    }
+    
+    int escolha = gerador.nextInt(total);
+    if (escolha < pesos[0]) {
+        jogador.minerar();
+    } else if (escolha < pesos[0] + pesos[1]) {
+        jogador.coletarMadeira();
+    } else {
+        jogador.construir();
+    }
   }
 }
